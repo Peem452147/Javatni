6db966cbce6ace26f09c59c2387752be1dd13b7c
Peem452147 <worakit.duangchan@gmail.com>
2025-12-15 15:58:53 +0700

    2513110367 LabSheet05

--- LabSheet05/src/Ex501.java
+++ LabSheet05/src/Ex501.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Ex501 {
+    public static boolean is_even(int number) {
+        return number % 2 == 0;
+    }
+public static String check_type_number(int number) {
+        if(number > 0){
+            return "Positive Number";
+        }else if (number < 0) {
+            return "Negtive Number";
+        }
+        return "Zero Number";
+}
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ener an integer");
+        int number = scanner.nextInt();
+        if (number == 0) {
+            if (is_even(number)) { //
+                System.out.println(number + " is Even and " +
+                        check_type_number(number));
+            } else {
+                System.out.println(number + " is Odd and " +
+                        check_type_number(number));
+            }
+        } else {
+            System.out.println(number + " is " + check_type_number(number));
+        }
+    }
 }
